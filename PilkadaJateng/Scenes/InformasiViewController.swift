db7c08fadbe4927e6b3d0dfac2351218b91f189e
Muhammad Muizzsuddin <arc.muiss@yahoo.com>
2018-03-25 09:31:46 +0700

    Update Informasi VC

--- PilkadaJateng/Scenes/InformasiViewController.swift
+++ PilkadaJateng/Scenes/InformasiViewController.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class InformasiViewController: UIViewController {
+    @IBOutlet weak var viewOutlets: InformasiView!
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        _setupPilkadaButton()
+        _setupCalonButton()
     }
+    
+    private func _setupPilkadaButton() {
+        let pilkadaButton = viewOutlets.pilkadaButton
+        pilkadaButton?.addTarget(self, action: .informasiPilkadaAction, for: .touchUpInside)
+    }
+    
+    private func _setupCalonButton() {
+        let calonButton = viewOutlets.calonButton
+        calonButton?.addTarget(self, action: .informasiCalonAction, for: .touchUpInside)
+    }
+    
+    @objc func _goToInformasiPilkada() {
+        performSegue(withIdentifier: "InformasiPilkadaViewController", sender: nil)
+    }
+    
+    @objc func _goToInformasiCalon() {
+        performSegue(withIdentifier: "InformasiCalonViewController", sender: nil)
+    }
+    
+    @IBAction func unwindInformasiViewController(_ segue: UIStoryboardSegue) {
+    }
+}
+
+fileprivate extension Selector {
+    static let informasiPilkadaAction = #selector(InformasiViewController._goToInformasiPilkada)
+    static let informasiCalonAction = #selector(InformasiViewController._goToInformasiCalon)
+}
+
+class InformasiView: UIView {
+    @IBOutlet weak var pilkadaButton: UIButton!
+    @IBOutlet weak var calonButton: UIButton!
 }
