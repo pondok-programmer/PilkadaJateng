6445998461ca1207b7a4ca52c50fc3a5c0ca4d09
Muhammad Muizzsuddin <arc.muiss@yahoo.com>
2018-04-09 20:27:46 +0700

    TimelineTahapanPilkada

--- PilkadaJateng/Scenes/InformasiTab/PJTahapan/PJTahapanPilkada.swift
+++ PilkadaJateng/Scenes/InformasiTab/PJTahapan/PJTahapanPilkada.swift
@@ -35,6 +35,7 @@ class PJTahapanPilkadaViewController: UIViewController {
     
     private func _setupTableView() {
         tableView.dataSource = self
+        tableView.delegate = self
         
         let bundle = Bundle(for: TimelineTableViewCell.self)
         let nibUrl = bundle.url(forResource: "TimelineTableViewCell", withExtension: "bundle")
@@ -56,13 +57,46 @@ extension PJTahapanPilkadaViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TahapanCell", for: indexPath) as! TimelineTableViewCell
         
-        let data = self.data[indexPath.section][indexPath.row]
+        let data = self.data[indexPath.section].sorted(by: { $0.awal < $1.awal } )[indexPath.row]
         cell.timelinePoint = TimelinePoint()
-        cell.timeline.frontColor = .red
-        cell.timeline.backColor = .black
+        
+        var frontColor: UIColor {
+            let isFirstItem = indexPath.row == 0
+            let isBeforeCurrentDate = Date() > data.awal
+            
+            guard !isFirstItem else {
+                return .clear
+            }
+            
+            if isBeforeCurrentDate {
+                return .black
+            }
+            
+            return .red
+        }
+        
+        cell.timeline.frontColor = frontColor
+        
+        var backColor: UIColor {
+            let isLastItem = indexPath.row >= self.data[indexPath.section].count - 1
+            let isBeforeCurrentDate = Date() > data.awal
+            
+            guard !isLastItem else {
+                return .clear
+            }
+            
+            if isBeforeCurrentDate {
+                return .black
+            }
+            
+            return .red
+        }
+        cell.timeline.backColor = backColor
+        
         cell.titleLabel.text = "\(data.awal.toString(format: "dd MMM yyyy")) sampai \(data.akhir.toString(format: "dd MMM yyyy"))"
         cell.descriptionLabel.text = data.detail
-        cell.lineInfoLabel.text = "\(dateDiff(firstDate: data.awal, secondDate: data.akhir)) hari"
+        let day = dateDiff(firstDate: Date(), secondDate: data.akhir)
+        cell.lineInfoLabel.text = "\(day < 0 ? 0 : day ) hari tersisa"
         return cell
     }
     
@@ -72,8 +106,17 @@ extension PJTahapanPilkadaViewController: UITableViewDataSource {
         // Replace the hour (time) of both dates with 00:00
         let date1 = calendar.startOfDay(for: firstDate)
         let date2 = calendar.startOfDay(for: secondDate)
-        
         let components = calendar.dateComponents([.day], from: date1, to: date2)
         return components.day ?? 0
+    }
+}
+
+extension PJTahapanPilkadaViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "Persiapan" : "Penyelenggaraan"
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 }
